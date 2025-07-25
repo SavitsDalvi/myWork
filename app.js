c9803a6c7fedf88cf4d31da2a21fc39667771010
Savita Dalvi <sav.dalvi88@gmail.com>
2025-07-25 16:56:30 +0530

    socket connection

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require('express')
+const path =require('path')
+const app =express()
+const PORT =process.env.PORT || 4000
+const server=app.listen(PORT, ()=>console.log(`server on port${PORT}`));
+
+const io=require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+// io.on('connection',(socket) =>{
+//     console.log(socket.id)
+// })
+let socketsConnected =new Set()
+
+io.on('connection',onConnected)
+
+function onConnected(socket){
+    console.log(socket.id)
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total',socketsConnected.size)
+    
+    socket.on('disconnect',()=>{
+        console.log('Socket Disconnected',socket.id);
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total',socketsConnected.size)
+
+        
+    })
+}
---
+++ public/main.js
@@ -0,0 +1,13 @@
+const socket =io()
+
+const clientsTotal =document.getElementById('clients-total')
+
+const messageContainer =document.getElementById('message-container')
+const nameInput =document.getElementById('name-input')
+const messageForm = document.getElementById('massge-form')
+const messageInput = document.getElementById('message-input')
+
+socket.on('clients-total', (data) =>{
+  clientsTotal.innerText = `Total Clients: ${data}`  
+    
+})
