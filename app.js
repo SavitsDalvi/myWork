5bacbd710bfa9a66552b7c5eedf21bbefe600537
Savita Dalvi <sav.dalvi88@gmail.com>
2025-07-30 12:14:33 +0530

    message broadcast

--- app.js
+++ app.js
@@ -28,4 +28,9 @@ function onConnected(socket){
 
         
     })
+    socket.on('message',(data)=>{
+        console.log(data);
+        
+        socket.broadcast.emit('chat-message',data)
+    })
 }
--- public/main.js
+++ public/main.js
@@ -7,7 +7,23 @@ const nameInput =document.getElementById('name-input')
 const messageForm = document.getElementById('massge-form')
 const messageInput = document.getElementById('message-input')
 
+messageForm.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    sendMessage()
+})
 socket.on('clients-total', (data) =>{
   clientsTotal.innerText = `Total Clients: ${data}`  
     
 })
+
+function sendMessage()
+{
+    console.log(messageInput.value);
+    const data={
+        name: nameInput.value,
+        message:messageInput.value,
+        dateTime:new Date()
+
+    }
+    socket.emit('message',data)
+}
